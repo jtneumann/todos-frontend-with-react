20e6cfdd9361e7e3f5c69eca078036a8d5def1ed
jtn <jtneumann@hotmail.com>
2018-08-27 23:03:12 +0000

    initial commit

---
+++ src/TodoList.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import TodoItem from './TodoItem';
+const APIURL = '/api/todos'
+
+class TodoList extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            todos: []
+        }
+    }
+    
+    componentWillMount(){
+      this.loadTodos();
+    }
+    
+    loadTodos(){
+        fetch(APIURL)
+        .then(resp => {
+            if(!resp.ok){
+                if(resp.status >= 400 && resp.status < 500) {
+                    return resp.json().that(data => {
+                        let err = {errorMessage: data.message}
+                        throw err;
+                    })
+                } else {
+                    let err = {errorMessage: 'Please try again later, server is not responding'};
+                    throw err;
+                }
+            }
+            return resp.json();
+        })
+        .then(todos => this.setState({todos}));  
+    }
+    
+    render(){
+        const todos = this.state.todos.map((t) => (
+                <TodoItem
+                    key={t._id}
+                    {...t}
+                    
+                />
+            ));
+            
+        return(
+            <div>
+                <ul>
+                    <h1>Todo List</h1>
+                    {todos}
+                </ul>
+            </div>
+            )
+    }
+}
+
+export default TodoList;
